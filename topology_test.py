3d5fdc1bf5324f8125902a94a7785b8f23680586
Ricky Ng-Adam <rngadam@gmail.com>
2013-01-17 21:12:23 +0800

    trying to reproduce our eventual configuration

---
+++ topology_test.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+#from __future__ import print_function
+import re
+import traceback
+import rsyslog
+import unittest
+import os
+import time
+import syslog
+import logging
+from logging.handlers import SysLogHandler
+import socket
+
+class TestRsyslogTopology:
+    OUTPUT_FILE = "/tmp/rsyslog-testing/server.log"
+    QUEUE = "/tmp/rsyslog-testing/srvrfwd.00000001"
+    QI = "/tmp/rsyslog-testing/srvrfwd.qi"
+
+    CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+    SERVER_CONF = CURRENT_DIR + '/server3.conf'
+    CLIENT_CONF = CURRENT_DIR + '/client3.conf'
+
+    uid = 0
+
+    def setUp(self):
+        # start client
+        # start server
+        rsyslog.deleteIgnoreError(self.OUTPUT_FILE)
+        rsyslog.deleteIgnoreError(self.QUEUE)
+        rsyslog.deleteIgnoreError(self.QI)
+
+        self.output = open(self.OUTPUT_FILE, 'w+')
+
+        self.server = rsyslog.Rsyslog(self.SERVER_CONF)
+        self.server.start()
+        self.server.waitOutput('worker IDLE, waiting for work')
+
+        self.client = rsyslog.Rsyslog(self.CLIENT_CONF)
+        self.client.start()
+        self.client.waitOutput('worker IDLE, waiting for work')
+
+    def tearDown(self):
+        self.client.stop()
+        self.server.stop()
+
+    def readLogs(self):
+        try:
+            if benchmark.client.process.returncode:
+                print 'client exited with %d' % benchmark.client.process.returncode
+            if benchmark.server.process.returncode:
+                print 'client exited with %d' % benchmark.server.process.returncode
+            # must read otherwise pipe blows up
+            client = benchmark.client.process.stdout.readall()
+            server = benchmark.server.process.stdout.readall()
+            print 'CLIENT:' + client,
+            print 'SERVER:' + server,
+        except:
+            pass
+
+
+if __name__ == '__main__':
+    benchmark = TestRsyslogTopology()
+    fh = None
+    SESSION = time.time()
+    DEBUG = True
+    count = 10000
+
+    print 'this session %d' % SESSION
+    try:
+        print "Benchmark setup with count %d" % count
+        benchmark.setUp()
+
+        syslog = SysLogHandler(
+            address=('127.0.0.1', 50140),
+            facility=SysLogHandler.LOG_USER,
+            socktype=socket.SOCK_DGRAM)
+        syslog.setLevel(logging.INFO)
+        logging.getLogger('').addHandler(syslog)
+        formatter = logging.Formatter('%(message)s')
+        syslog.setFormatter(formatter)
+
+        for i in xrange(0, count):
+            logging.info('uid:%d session:%d' % (i, SESSION))
+            print i,
+            benchmark.readLogs()
+
+        print "Sent %d" % count
+        received = 0
+        fh = open(benchmark.OUTPUT_FILE, 'r')
+
+        start = time.time()
+        while count > received:
+            line = fh.readline().rstrip()
+
+            if len(line) > 0:
+                if DEBUG:
+                    print line
+                s = re.search("uid:(\d+)", line)
+                if s:
+                    uid = int(s.group(1))
+                    if DEBUG:
+                        print '#%d[%d]' % (received, uid),
+                    received = received + 1
+            benchmark.readLogs()
+
+        delta = time.time() - start
+        per_thousand = (delta/count) * 1000
+        per_second = count/delta
+        print "elapsed: %.3f s per_thousand:%.3f per_second:%.3f" % (delta, per_thousand, per_second)
+    except Exception, e:
+        print 'Main loop exception ', e
+        traceback.print_exc()
+    finally:
+        print 'cleaning up'
+        benchmark.tearDown()
+        if fh:
+            fh.close()
